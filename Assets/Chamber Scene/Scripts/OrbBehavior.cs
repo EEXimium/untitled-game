2e53dc163dcbf65016ed3752b04ae5ddf528283a
merzifonlu1 <kerem_sarac@windowslive.com>
2023-08-02 16:51:13 +0300

    Chamber Scene #3
    ----------------------- Optimization and Last Touches -----------------------
    
    Yerleşik Chamberlar'a bitişik Chamberlar artık Highlight oluyor.
    Orblar Yalnızca Highlight kısımlara konabiliyor.
    C# Script'leri gereksiz yere fazladan oluşturulmuştu. (her orba bir tane) Artık tek script tüm orblarda kullanılabilir.

--- Assets/Chamber Scene/Scripts/OrbBehavior.cs
+++ Assets/Chamber Scene/Scripts/OrbBehavior.cs
@@ -1,32 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class cimorb : MonoBehaviour
+public class OrbBehavior : MonoBehaviour
 {
     private bool dragging;
     private CircleCollider2D Ccoll;
 
     [SerializeField] private Vector3 AnchorPoint;
 
-    public GameObject runrock;
-
-    public GameObject CimPrefab;
+    public GameObject SelfOrb;
+    public GameObject ChamberPrefab;
 
     private void Start()
     {
         dragging = false;
-        Ccoll = GetComponent<CircleCollider2D>();        
+        Ccoll = GetComponent<CircleCollider2D>();
     }
 
     void Update()
     {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);  // Get mouse pos
 
         if (Input.GetMouseButtonDown(0))
         {
-            if (Ccoll == Physics2D.OverlapPoint(mousePos))
+            if (Ccoll == Physics2D.OverlapPoint(mousePos))  // if mouse pos on collider
             {
                 dragging = true;
             }
@@ -34,11 +32,11 @@ public class cimorb : MonoBehaviour
 
         if (dragging)
         {
-            this.transform.position = mousePos;
+            this.transform.position = mousePos;         // obje ==> mouse pos
         }
         else
         {
-            StartCoroutine(ReturnToBase());
+            StartCoroutine(ReturnToBase());      // return holder
         }
 
         if (Input.GetMouseButtonUp(0))
@@ -47,12 +45,12 @@ public class cimorb : MonoBehaviour
         }
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)        // Orb Creating Chamber
     {
         if (collision.gameObject.CompareTag("Slot") && !dragging)
         {
-            Destroy(runrock);
-            Instantiate(CimPrefab, collision.gameObject.transform.position, collision.gameObject.transform.rotation);
+            Destroy(SelfOrb);
+            Instantiate(ChamberPrefab, collision.gameObject.transform.position, collision.gameObject.transform.rotation);
             Destroy(collision.gameObject);
         }
     }
