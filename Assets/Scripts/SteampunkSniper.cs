0f1b15d47c3d690a00eba5efc3aa60bfa7fe4f86
merzifonlu1 <kerem_sarac@windowslive.com>
2023-08-20 22:58:11 +0300

    Silah Eklenmesi (Kerem) #2
    sağ tık zoom
    sol tık ateş
    
    ----BUGS----
    kamera sürekli kaymaya devam ediyor o kayınca mouse da kayıyor mouse kayınca kamera kayıyor.

--- Assets/Scripts/SteampunkSniper.cs
+++ Assets/Scripts/SteampunkSniper.cs
@@ -1,20 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.Examples;
 using UnityEngine;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
 public class SteampunkSniper : MonoBehaviour
 {
-    public Transform character; // Reference to the character's Transform
-    public float orbitRadius = 1.5f;
-
+    public Transform character;
     public GameObject BulletPrefab;
     public Transform firePoint;
-    public float fireRate;
-    bool canshoot = true;
     public SpriteRenderer gun;
+
+    public float orbitRadius = 1.5f;  // silahýn karaktere olan uzaklýðý ya da yakýnlýðý
+    public float fireRate; 
+
+    public bool canshoot = true;
+  
+
+    // Camera Movement
+    private new Camera camera;
+    [SerializeField] private SpriteRenderer camlimitrender;
+    private float limitminX, limitmaxX, limitminY, limitmaxY;
+    Vector3 StartMousePos;
+
+    private void Start()
+    {
+        camera = Camera.main;
+    }
 
     private void Update()
     {
+        // --------------- Zoom -------------------
+        if (Input.GetMouseButton(1))
+        {
+            Onzoom();
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            Offzoom();
+        }
+
+
+
         // Calculate the angle based on mouse position
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3 directionToMouse = mousePosition - character.position;
@@ -27,25 +54,55 @@ public class SteampunkSniper : MonoBehaviour
         transform.position = orbitPosition;
         transform.rotation = Quaternion.Euler(0, 0, currentAngle);
 
-        if (directionToMouse.x < 0)
-        {
+        if (directionToMouse.x < 0)  
             gun.flipY = true;
-        }
-        else
-        {
-            gun.flipY = false;
-        }
+
+        else 
+            gun.flipY = false;                          
 
         Vector3 aimDirection = (mousePosition - character.position).normalized;
         float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, aimAngle);
 
-        if (Input.GetMouseButton(0) && canshoot)
+        if (Input.GetMouseButtonDown(0) && canshoot)
         {
             StartCoroutine(Shoot());
         }
 
     }
+
+    // --------------- Zoom -------------------
+    private void Onzoom()
+    {
+        limitminX = camlimitrender.transform.position.x - camlimitrender.bounds.size.x;
+        limitmaxX = camlimitrender.transform.position.x + camlimitrender.bounds.size.x;
+
+        limitminY = camlimitrender.transform.position.y - camlimitrender.bounds.size.y;
+        limitmaxY = camlimitrender.transform.position.y + camlimitrender.bounds.size.y;
+
+
+        Vector3 mouse2Position = camera.ScreenToWorldPoint(Input.mousePosition);     
+        camera.transform.position = CameraLimit(Vector3.Slerp(camera.transform.position, mouse2Position, 5f * Time.deltaTime));
+
+    }
+    private void Offzoom()
+    {
+        Debug.Log("returning");
+        Vector3 basepos = new Vector3(character.position.x, character.position.y, camera.transform.position.z);
+        camera.transform.position = CameraLimit(basepos);
+
+    }
+
+    private Vector3 CameraLimit(Vector3 targetPosition)
+    {
+
+
+        float newX = Mathf.Clamp(targetPosition.x, limitminX, limitmaxX);
+        float newY = Mathf.Clamp(targetPosition.y, limitminY, limitmaxY);
+
+        return new Vector3(newX, newY, targetPosition.z);
+    }
+    // ---------------------------------------
 
     public IEnumerator Shoot()
     {
