109f561dda60bf82a15ab70ef9788a92cf65ee4e
kaanardaa <kaansagan@gmail.com>
2023-08-28 15:03:50 +0300

    Weapon Equip and Inventory #2 (Kaan)
    Items are now pickable
    They are visible in the inventory with their image and quantity

--- Assets/In-Game Scene/Scripts/InventoryManager.cs
+++ Assets/In-Game Scene/Scripts/InventoryManager.cs
@@ -7,6 +7,7 @@ public class InventoryManager : MonoBehaviour
     
     public GameObject InventoryMenu; 
     private bool menuActivated;
+    public ItemSlot[] itemSlot; 
     
     // Start is called before the first frame update
     void Start()
@@ -31,4 +32,21 @@ public class InventoryManager : MonoBehaviour
            menuActivated = true;
         }
     }
+
+    public void AddItem(string itemName, int quantity, Sprite itemSprite)
+    {
+       Debug.Log("itemName: " + itemName + " quantity: " + quantity + " itemSprite: " + itemSprite);
+
+       for (int i = 0; i < itemSlot.Length; i++)
+       {
+           if(itemSlot[i].isFull == false)
+           {
+                itemSlot[i].AddItem(itemName, quantity, itemSprite);
+                return;
+           }
+       }
+    }
+
+
+
 }
