21f0c64c28c528fa1c9a309338c7e805f2670341
Yağız Efe Atasever <ataseverefeyagiz@gmail.com>
2023-10-02 18:18:09 +0300

    LookAtMove
    :sparkles: FIXED :sparkles:
    :point_right: 8 directional character animations work properly. Character will turn to move direction

--- Assets/In-Game Scene/Scripts/Player/PlayerMovement.cs
+++ Assets/In-Game Scene/Scripts/Player/PlayerMovement.cs
@@ -15,10 +15,22 @@ public class PlayerMovement : MonoBehaviour
     public codex CodexScript;
     public GameObject UIDoc;
     private Animator animator;
+    private string currentState;
 
-    private bool diablo = false;
+    //private bool diabloState = false;
 
-
+    //ANIM STATES
+    const string Idle = "Idle";
+    const string Run = "Run";
+    const string Walk_Left = "Walk_Left";
+    const string Walk_Right = "Walk_Right";
+    const string Walk_Up = "Walk_Up";
+    const string Walk_Up_Left = "Walk_Up_Left";
+    const string Walk_Up_Right = "Walk_Up_Right";
+    const string Walk_Down = "Walk_Down";
+    const string Walk_Down_Left = "Walk_Down_Left";
+    const string Walk_Down_Right = "Walk_Down_Right";
+    const string diabloAnim = "diablo";
 
     void Start()
     {
@@ -32,8 +44,6 @@ public class PlayerMovement : MonoBehaviour
         currentPos = rb.position;
         InputVector.x = Input.GetAxisRaw("Horizontal");
         InputVector.y = Input.GetAxisRaw("Vertical");
-        InputVector.Normalize(); //Diagonal hareketin bozuk hýzlý olmamasý için
-        InputVector = Vector2.ClampMagnitude(InputVector, 1);    // Diagonal movement 1,4 => 1
 
         //------Look at cursor--------
         //Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -43,38 +53,86 @@ public class PlayerMovement : MonoBehaviour
         //direction = Vector2.ClampMagnitude(direction, 1f);
         //direction = new Vector2(Mathf.Clamp(direction.x, -1f, 1f), Mathf.Clamp(direction.y, -1f, 1f));
 
-        if (Input.GetKey(KeyCode.LeftShift))      
-        { newPos = currentPos + (InputVector * (movespeed + sprintspeed) * Time.fixedDeltaTime); }
+        //-----------------------------------------------
 
-        else
-        { newPos = currentPos + (InputVector * movespeed * Time.fixedDeltaTime); }
-
-        if (Input.GetKeyDown(KeyCode.H) && !diablo)
-        {
-            diablo = true;
-            animator.SetTrigger("FKeyPressed");
-        }
-
-        rb.MovePosition(newPos);
-
-        animator.SetFloat("Horizontal", InputVector.x);
-        animator.SetFloat("Vertical", InputVector.y);
+        ////animator.SetFloat("Horizontal", InputVector.x);
+        ////animator.SetFloat("Vertical", InputVector.y);
 
         //animator.SetFloat("MouseHorizontal", direction.x);
         //animator.SetFloat("MouseVertical", direction.y);
         //animator.SetFloat("Speed", InputVector.sqrMagnitude);
     }
 
-    public void endDiablo()
-    {
-        diablo = false;
-    }
-
     private void FixedUpdate()
     {
-        //rb.MovePosition(newPos);
-        //rb.velocity = InputVector * movespeed;
+        InputVector.Normalize(); //Diagonal hareketin bozuk hýzlý olmamasý için
+        InputVector = Vector2.ClampMagnitude(InputVector, 1);    // Diagonal movement 1,4 => 1
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        { 
+            newPos = currentPos + (InputVector * (movespeed + sprintspeed) * Time.fixedDeltaTime); 
+        }
+        else
+        { 
+            newPos = currentPos + (InputVector * movespeed * Time.fixedDeltaTime); 
+        }
+
+        rb.MovePosition(newPos);
+
+        //ANIM CHANGES
+
+        if(InputVector.x >= 0.1f && InputVector.y == 0)
+        {
+            ChangeAnimationState(Walk_Right);
+        }
+        else if(InputVector.x <= -0.1f && InputVector.y == 0)
+        {
+            ChangeAnimationState(Walk_Left);
+        }
+        else if(InputVector.x == 0 && InputVector.y >= 0.1)
+        {
+            ChangeAnimationState(Walk_Up);
+        }
+        else if (InputVector.x >= 0.1f && InputVector.y >= 0.1)
+        {
+            ChangeAnimationState(Walk_Up_Right);
+        }
+        else if (InputVector.x <= -0.1f && InputVector.y >= 0.1)
+        {
+            ChangeAnimationState(Walk_Up_Left);
+        }
+        else if (InputVector.x == 0 && InputVector.y <= -0.1)
+        {
+            ChangeAnimationState(Walk_Down);
+        }
+        else if (InputVector.x >= 0.1f && InputVector.y <= -0.1)
+        {
+            ChangeAnimationState(Walk_Down_Right);
+        }
+        else if (InputVector.x <= -0.1f && InputVector.y <= -0.1)
+        {
+            ChangeAnimationState(Walk_Down_Left);
+        }
+        else
+        {
+            ChangeAnimationState(Idle);
+        }
+
+        /*
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            ChangeAnimationState(diabloAnim);
+        }
+        */
+
     }
+
+    /*
+    public void endDiablo()
+    {
+        diabloState = false;
+    }
+    */
 
     /*
     private void OnTriggerEnter2D(Collider2D other)
@@ -84,8 +142,6 @@ public class PlayerMovement : MonoBehaviour
             string collectedWeaponName = other.gameObject.name; // Assuming the weapon's GameObject name matches its weapon name
             collectedWeapons.Add(collectedWeaponName);
             unlockedWeapons[collectedWeaponName] = true;
-
-            
 
             // Optional: Deactivate the collected weapon object
             other.gameObject.SetActive(false);
@@ -107,5 +163,18 @@ public class PlayerMovement : MonoBehaviour
             // Deactivate the collected weapon object
             other.gameObject.SetActive(false);
         }
+    }
+
+    void ChangeAnimationState(string newState)
+    {
+        //halihazýrda oynayan animasyonun kendini kesmesine engel ol
+        if (currentState == newState)
+            return;
+
+        //yeni anim oynat
+        animator.Play(newState);
+
+        //current state güncelle çünkü deðiþti
+        currentState = newState;
     }
 }
