174ae8c9ee2d995878d249c81fc7a2b21661fc03
Talib Yesildal <talibyesildal@yaani.com>
2023-10-09 00:42:26 +0300

    :sparkles: Health System Update :sparkles:
    :heart: When player get hit sprite blinks and says UGhh!.
    :heart: DisplayText method created.
    :heart: Inspector edited.
    :heart: Health system works only on bavsPlayground because not added to prefab until review.

--- Assets/In-Game Scene/Scripts/Player/HealthSystem/PlayerHealth.cs
+++ Assets/In-Game Scene/Scripts/Player/HealthSystem/PlayerHealth.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public HealthBar healthBar;
+    private SpriteRenderer charSpriteR;
+    [Header("Variables")]
     public int maxHealth = 4;
     public int currentHealth;
-
-    public HealthBar healthBar;
+    [SerializeField] private Color damageColor = Color.red;
 
     void Start()
     {
         currentHealth = maxHealth;  
         healthBar.SetMaxHealth(maxHealth);
+        charSpriteR = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -20,7 +26,16 @@ public class PlayerHealth : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             TakeDamage(1);
+            StartCoroutine(ColorShift());
+            DisplayText();
         }
+    }
+
+    private IEnumerator ColorShift()
+    {
+        charSpriteR.color = damageColor;
+        yield return new WaitForSeconds(0.1f);
+        charSpriteR.color = Color.white;
     }
 
     public void TakeDamage(int damage) 
@@ -33,5 +48,20 @@ public class PlayerHealth : MonoBehaviour
     {
         currentHealth += healcount;
         healthBar.SetHealth(currentHealth);
+    }
+
+
+    //UsableScript!!
+    public string textToDisplay = "UGhh!";
+    public float deleteTime = 0.2f;
+
+    public void DisplayText()
+    {
+        GameObject textObject = Instantiate(new GameObject("text"), Vector3.zero, Quaternion.identity);
+        TextMesh textMesh = textObject.AddComponent<TextMesh>();
+        textMesh.text = textToDisplay;
+        textObject.transform.Translate(0,0,-1);
+        Object.Destroy(GameObject.Find("text"));// method creates a blank object somehow this is for it.
+        Object.Destroy(textObject, deleteTime);
     }
 }
