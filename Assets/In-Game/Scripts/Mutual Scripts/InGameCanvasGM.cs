7a0865817f108f924a5e6cc50a3cfb48aa2df144
Yağız Efe Atasever <ataseverefeyagiz@gmail.com>
2024-03-02 12:10:58 +0300

    Death & Overhaul 2
    update:
    - death menü fonksiyonel
    
    overhaul:
    - bazı klasör isimleri ve içerikleri kullanıma daha uygun olarak değiştirildi, kullanım ve navigasyon basitleştirildi
    
    bug:
    - death menüdeki "return to save" buglı

---
+++ Assets/In-Game/Scripts/Mutual Scripts/InGameCanvasGM.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Search;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class InGameCanvasGM : MonoBehaviour
+{
+    //[Header("Menu Navigation")]
+    //[SerializeField] private SaveSlotsMenu saveSlotsMenu;
+
+    [Header("Menu Buttons")]
+    //[SerializeField] private Button newGameButton;
+    //[SerializeField] private Button continueGameButton;
+    //[SerializeField] private Button LoadGameButton;
+    [SerializeField] private GameObject PauseMenu;
+    [SerializeField] private GameObject HUD;
+    [SerializeField] private GameObject DeathMenu;
+    [SerializeField] private GameObject Player;
+    [SerializeField] private SaveSlotsMenu saveSlotsMenu;
+
+
+    private void Update()
+    {
+        // ESC tuþu ile oyunu durdur.
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            FreezeGame();
+            PauseMenu.SetActive(true);
+            HUD.SetActive(false);
+        }
+
+        if (Player.GetComponent<PlayerHealth>().isDead)
+        {
+            FreezeGame();
+            DeathMenu.SetActive(true);
+            HUD.SetActive(false);
+        }
+    }
+
+    public void returnToSave()
+    {
+        DeathMenu.SetActive(false);
+        saveSlotsMenu.ActivateMenu(true);
+        
+    }
+
+    public void FreezeGame()  { Time.timeScale = 0; }
+    public void UnFreezeGame() { Time.timeScale = 1; }
+    
+    public enum Scenes { MainMenu, Outside }
+    public Scenes ChooseScene;
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(ChooseScene.ToString());
+    }
+
+    
+}
