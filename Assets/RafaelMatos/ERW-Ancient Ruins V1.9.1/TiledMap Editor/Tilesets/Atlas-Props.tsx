dbb34b6165e5e24d1b5864327fa8f718365d22b5
Kerem <kerem_sarac@windowslive.com>
2024-03-06 16:15:42 +0300

    Asset Import
    Study of Rule Tiles.

---
+++ Assets/RafaelMatos/ERW-Ancient Ruins V1.9.1/TiledMap Editor/Tilesets/Atlas-Props.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Atlas-Props" tilewidth="32" tileheight="32" tilecount="2550" columns="50">
+ <image source="../../Props/Atlas-Props.png" width="1600" height="1632"/>
+</tileset>
